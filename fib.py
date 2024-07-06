4f4f8f40a6e9bacfe98aa90a22cb37386f1d5604
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-07-06 14:13:05 -0600

    comment

--- fib.py
+++ fib.py
@@ -1,5 +1,4 @@
 # validation
-
 while True:
     try:
         totalNum = int(input("Enter the number of digits you want to generate (must be 2 or greater): "))
@@ -10,16 +9,25 @@ while True:
     except ValueError:
         print("Please enter a valid number")
 
+# initialize the sequence
 seq = [0,1]
 
+
 if totalNum > 2:
+    
+    #account for the first two numbers
     totalNum -= 2
     a = 0
     b = 1
     
+    # generate the sequence
     for i in range(totalNum):
         fib = seq[a] + seq[b]
+        
+        # append the new number to the sequence
         seq.append(fib)
+        
+        # increment the pointers
         a += 1
         b += 1
 
